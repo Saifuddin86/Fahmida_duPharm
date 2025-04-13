9f3082513da3f3cea4bb54ad8c90e057e6130223
saifuddin-statlab <saif86.ctg@gmail.com>
2025-04-14 02:51:26 +0600

    Cleaning Multiple response data

--- Multiple responses.R
+++ Multiple responses.R
@@ -31,3 +31,36 @@ summary(glm_model)
 
 
 tbl_regression(glm_model,exponentiate = TRUE)
+
+
+
+
+### A great way to clean Multiple Responses data
+
+library(stringr)
+library(readxl)
+library(tidyverse)
+str_squish("  Hello    world   R   is   great!  ")
+
+text_dat <- read_excel("D:/2.Statlab/Domestic C/Dr. Rashed Sir/Fahmida Akter_fahmida-2017914088@pharmacy.du.ac.bd/Copy of Personal_Level_screening_for_COPD(1).xlsx",
+                       sheet = "Sheet6", skip = 1)
+
+glimpse(text_dat)
+
+
+text_dat$`Which medication you are taking for COPD (upated)?`<-str_squish(text_dat$`Which medication you are taking for COPD (upated)?`)
+
+
+text_dat %>% select(`Which medication you are taking for COPD (upated)?`) 
+
+
+#library(tidyr)
+#df_clean <- df %>%
+#  separate_rows(responses, sep = ",\\s*")  # Splits by comma and optional space
+
+
+
+text_dat %>% 
+  separate_rows(`Which medication you are taking for COPD (upated)?`, sep = ",\\s*") %>% 
+  count(`Which medication you are taking for COPD (upated)?`
+        ) %>% knitr::kable()
